6ae65b2d865a25d918aafd16144f15d45857ff11
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2023-04-18 20:13:48 +0700

    weatherapi.com changes

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 const express = require("express");
-const fetch = require("node-fetch");
-require("dotenv").config();
+const https = require("https");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const API_KEY = process.env.OPENWEATHERMAP_API_KEY;
+const port = process.env.PORT || 3000;
 
-app.get("/weather", async (req, res) => {
-  const location = req.query.location;
+const API_ENDPOINT = "https://api.weatherapi.com/v1/current.json";
+const API_KEY = process.env.API_KEY;
 
-  if (!location) {
-    return res.status(400).send({ error: "Location is required" });
-  }
+app.get("/weather/:city", (req, res) => {
+  const { city } = req.params;
+  const url = `${API_ENDPOINT}?key=${API_KEY}&q=${city}`;
 
-  try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=imperial`;
-    const response = await fetch(url);
+  https
+    .get(url, (response) => {
+      let body = "";
 
-    if (!response.ok) {
-      const errorText = await response.text();
-      console.error(`API response error: ${errorText}`);
-      return res.status(500).send({ error: "API response not OK" });
-    }
+      response.on("data", (chunk) => {
+        body += chunk;
+      });
 
-    const data = await response.json();
-    const temp = data.main?.temp;
-    const humidity = data.main?.humidity;
+      response.on("end", () => {
+        const data = JSON.parse(body);
 
-    if (!temp) {
-      console.error("Temperature not found in API response");
-      return res
-        .status(500)
-        .send({ error: "Temperature not found in API response" });
-    }
+        if (response.statusCode !== 200) {
+          return res
+            .status(400)
+            .send({ error: `Failed to fetch weather data for ${city}` });
+        }
 
-    res.send({ temp, humidity });
-  } catch (err) {
-    console.error(`API call error: ${err}`);
-    res.status(500).send({ error: "Server error" });
-  }
+        const { temp_c, feelslike_c, condition } = data.current;
+
+        res.send({
+          city,
+          temperature: temp_c,
+          feelsLike: feelslike_c,
+          description: condition.text,
+        });
+      });
+    })
+    .on("error", (error) => {
+      res.status(400).send({ error: error.message });
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
